005cdcc54db13d25a335e978ccb00dc73c958f8f
Kunal Jadhav <94116481+Kunal-Jadhav-1@users.noreply.github.com>
2024-09-09 17:18:15 +0530

    responsive header

--- src/components/Foods.js
+++ src/components/Foods.js
@@ -13,7 +13,7 @@ const Foods = ({ resData, listOfRestaurants }) => {
 
     return (
         <div className="relative m-4 p-2 w-full sm:w-64 h-auto bg-gray-150 rounded-lg hover:shadow-gray-700 hover:shadow-2xl hover:border border-tertiary hover:bg-tertiary group  md:text-base text-sm sm:items-start">
-            <img className="h-36 w-60 my-1 rounded-md cursor-pointer" src={imageId} alt={name} />
+            <img className="h-36 w-60 my-1 rounded-md cursor-pointer mx-auto" src={imageId} alt={name} />
             <h3 className="my-2 font-sans text-center font-bold text-ellipsis overflow-hidden whitespace-nowrap">{name}</h3>
             <h5 className="font-sans text-center">Price - ₹{price / 100} | Rating - {rating} 🌟</h5>
             {favourite === 1 && (
