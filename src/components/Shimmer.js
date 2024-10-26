99411d34153e25176fed6358e2a9a346d9d53943
Kunal Jadhav <94116481+Kunal-Jadhav-1@users.noreply.github.com>
2024-10-26 06:08:54 +0530

    Commit 13

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -3,124 +3,104 @@ import logo_load from '../images/logo_load.png';
 const Shimmer = () => {
     return (
         <div className="flex flex-wrap m-2 mx-14 ">
-            <div className="m-3 p-2 w-64 h-96 animate-pulse bg-gray-100 rounded-md">
+            <div className="m-3 p-2 w-64 h-80 animate-pulse bg-tertiary rounded-md">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
-                <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 animate-pulse rounded-md">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary animate-pulse rounded-md">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
-                <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
-                <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
-            <div className="m-3 p-2 w-64 h-96 bg-gray-100 rounded-md animate-pulse">
+            <div className="m-3 p-2 w-64 h-80 bg-tertiary rounded-md animate-pulse">
                 <div className="h-36 w-60 my-1 rounded-md cursor-pointer bg-gray-300" ></div>
                 <h3 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h3>
-                <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
                 <h5 className="rounded-md text-center m-6 text-gray-900 bg-gray-300 h-10 w-200 ml-22"></h5>
             </div>
             
