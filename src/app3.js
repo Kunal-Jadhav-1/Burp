a627d28a3f188a9ab84075524b59e572a5b706c8
Kunal Jadhav <94116481+Kunal-Jadhav-1@users.noreply.github.com>
2024-12-09 15:59:50 +0530

    signin

--- src/app3.js
+++ src/app3.js
@@ -1,3 +1,4 @@
+import 'dynamic-import-polyfill';
 import React, { lazy, Suspense, useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
